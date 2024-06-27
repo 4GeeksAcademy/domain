c468879cd672d7006a90fa04c16d4d860d4b9bab
FaithIsWork11 <faithiswork@duck.com>
2024-06-27 14:22:15 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+function generate(ending) {
+  let domain =
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+    adj[Math.floor(Math.random() * adj.length)] +
+    noun[Math.floor(Math.random() * noun.length)];
+  return (domain = domain + ending);
+}
+const elem = document.getElementById("com");
+elem.innerHTML = generate(".com");
+const elem1 = document.getElementById("net");
+elem1.innerHTML = generate(".net");
+const elem2 = document.getElementById("org");
+elem2.innerHTML = generate(".org");
